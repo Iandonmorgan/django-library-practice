0dc52f6853e19d284e4e4a246b6357618100104a
Landon Morgan <morganlandon@gmail.com>
2020-05-04 13:22:00 -0500

    Django HTML Templates practice

--- libraryproject/libraryapp/views/connection.py
+++ libraryproject/libraryapp/views/connection.py
@@ -1,2 +1,2 @@
 class Connection:
-    db_path = "/absolute/path/to/library-management/libraryproject/db.sqlite3"
+    db_path = "/Users/landonmorgan/workspace/backend/library-management/libraryproject/db.sqlite3"
