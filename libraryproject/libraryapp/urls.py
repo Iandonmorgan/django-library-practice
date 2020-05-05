04f096944b0353f4990d153a21b967bacacfc298
Landon Morgan <morganlandon@gmail.com>
2020-05-05 09:29:15 -0500

    book 2 chapter 3 practice complete

--- libraryproject/libraryapp/urls.py
+++ libraryproject/libraryapp/urls.py
@@ -1,11 +1,13 @@
-from django.urls import path
+from django.urls import include, path
 from .views import *
 
 app_name = "libraryapp"
 
 urlpatterns = [
-    path('', book_list, name='home'),
+    path('', home, name='home'),
     path('books/', book_list, name='books'),
     path('librarians/', list_librarians, name='librarians'),
     path('libraries/', list_libraries, name='libraries'),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('logout/', logout_user, name='logout'),
 ]
